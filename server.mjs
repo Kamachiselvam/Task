0c3bbebed9fbb9156731f0c3dd165bb7577025c4
Kamachiselvam <kamachiselvam94@gmail.com>
2026-06-03 22:27:23 +0530

    created server.mjs

---
+++ server.mjs
@@ -0,0 +1,28 @@
+import express from "express";
+import userRouters from "./routers/userRouter.mjs";
+import taskRouters from './routers/taskRouter.mjs';
+import { user } from "./data/userdata.mjs";
+import { task } from "./data/taskdata.mjs";
+import mongoose from "mongoose";
+const app=express()
+const port=9000;
+app.use(express.json())
+app.use('/api',userRouters)
+app.use('/api',taskRouters)
+
+
+mongoose.connect('mongodb://localhost/login')
+.then(()=>console.log("Db connected"))
+.catch((err)=>console.log(err))
+
+mongoose.connect('mongodb://localhost/task')
+.then(()=>console.log("Db connected"))
+.catch((err)=>console.log(err))
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`The server running on ${port}`)
+})
