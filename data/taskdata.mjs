0f1dd844dbb0e4a77738aee9055a35fd6096deb5
Kamachiselvam <kamachiselvam94@gmail.com>
2026-06-05 23:39:10 +0530

    taskdata.mjs

---
+++ data/taskdata.mjs
@@ -0,0 +1,6 @@
+export const task=[
+    {id:1,title:"learn node js",description:"finish backend module",status:"pending",priority:"high",userid:"6a1eb944666286837a04dde8"},
+    {id:2,title:"learn express js",description:"finish frontend module",status:"pending",priority:"medium",userid:"6a1eb944666286837a04dde8"},
+    {id:3,title:"learn node js",description:"finish backend module",status:"pending",priority:"high",userid:"6a1eb944666286837a04dde8"},
+    
+]
