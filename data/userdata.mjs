1386d0352b0b19bb6ed9f2ca2fcaeba8ab56e938
Kamachiselvam <kamachiselvam94@gmail.com>
2026-06-05 23:39:47 +0530

    userdata.mjs

---
+++ data/userdata.mjs
@@ -0,0 +1,4 @@
+export const user=[{id:1,name:"arun",email:"arun123@gmail.com",password:"Arun123@"},
+    {id:2,name:"anjali",email:"anjali123@gmail.com",password:"Anjali123@"},
+    {id:1,name:"anu",email:"anu123@gmail.com",password:"Anu123@"},
+]
